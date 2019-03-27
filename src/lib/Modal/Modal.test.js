8fa77dafb67890d1db30b6c027ddb640c1bbc293
Kevin Simpson <ksimpso@ksimpso-us-la.am.trimblecorp.net>
2019-03-27 15:58:02 -0600

    Add initial Modal JSX and stories

---
+++ src/lib/Modal/Modal.test.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { shallow } from "enzyme";
+
+import Modal from "./Modal";
+
+describe("Modal", () => {
+  let wrapper;
+  let mockOnClickOne;
+  let mockOnClickTwo;
+  let mockClose;
+});
