7ca5b2c40f860a14a6e7cabb7f68fefab8c7633f
Kevin Simpson <ksimpso@ksimpso-us-la.am.trimblecorp.net>
2019-03-27 10:32:19 -0600

    Move setupTest.js to /src

--- src/lib/Button/Button.test.js
+++ src/lib/Button/Button.test.js
@@ -1,9 +1,4 @@
 import React from "react";
-const Enzyme = require("enzyme");
-const EnzymeAdapter = require("enzyme-adapter-react-16");
-
-// Setup enzyme's react adapter
-Enzyme.configure({ adapter: new EnzymeAdapter() });
 
 import { shallow } from "enzyme";
 
