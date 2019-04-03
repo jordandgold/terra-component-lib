3b4636dddc3656cfc68dc818568d8a6a9238dceb
Jordan Gold <jordandgold@gmail.com>
2019-04-03 10:14:11 -0600

    add notification styling

--- src/lib/Notification/Notification.js
+++ src/lib/Notification/Notification.js
@@ -4,11 +4,13 @@ import PropTypes from "prop-types";
 
 class Notification extends Component {
   render() {
-    const { onClick, text } = this.props;
+    const { onClick, text, type } = this.props;
     return (
-      <section className="notification">
-        <p>{text}</p>
-        <button className="notification-close-icon" onClick={onClick} />
+      <section className={`ter-notification ter-notification--${type}`}>
+        <button className="ter-notification__close" onClick={onClick} />
+        <div class="ter-notification__body">
+          <p>{text}</p>
+        </div>
       </section>
     );
   }
@@ -18,5 +20,6 @@ export default Notification;
 
 Notification.propTypes = {
   text: PropTypes.string.isRequired,
+  type: PropTypes.string.isRequired,
   onClick: PropTypes.func.isRequired
 };
