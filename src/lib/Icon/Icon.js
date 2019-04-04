ab80827985067e93d5d6e6c5d34f40f112862894
Jordan Gold <jordandgold@gmail.com>
2019-04-04 14:02:58 -0600

    update icon

---
+++ src/lib/Icon/Icon.js
@@ -0,0 +1,15 @@
+import React from "react";
+import PropTypes from "prop-types";
+import "./Icon.scss";
+
+const Icon = props => {
+  const { icon } = props;
+
+  return (
+    <div>
+        Icon
+    </div>
+  );
+};
+
+export default Icon;
