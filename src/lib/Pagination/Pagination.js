9816f9cea9d866c31d2d9802845dd4019ef3028e
Kevin Simpson <ksimpso@ksimpso-us-la.am.trimblecorp.net>
2019-04-05 14:56:56 -0600

    Add pagination logic

--- src/lib/Pagination/Pagination.js
+++ src/lib/Pagination/Pagination.js
@@ -1,41 +1,94 @@
 import React, { Component } from "react";
 import "./Pagination.scss";
 import PropTypes from "prop-types";
+import Icon from "../Icon/Icon";
 
 class Pagination extends Component {
   generatePageListItems = () => {
-    return this.props.pages.map((page, index) => {
-      return (
-        <li key={`list-item-${index}`} className="ter-pagination__list-item">
-          <a
-            className={`ter-pagination__list-item ${
-              page.isActive ? "is-active" : ""
-            }`}
-            href={`/${page.link}`}
-          >
-            {page.pageNumber}
-          </a>
-        </li>
-      );
-    });
+    const totalPages = this.props.pages; //totalPages.length()
+    const activePage = this.props.activePage + 1;
+
+    let pages = [];
+
+    if (totalPages === 2) {
+      for (let i = 0; i < 2; i++) {
+        pages.push(
+          <Page
+            key={`list-item-${i + 1}`}
+            activePage={activePage}
+            page={i + 1}
+          />
+        );
+      }
+    }
+
+    if (totalPages === 3) {
+      for (let i = 0; i < 3; i++) {
+        pages.push(
+          <Page
+            key={`list-item-${i + 1}`}
+            activePage={activePage}
+            page={i + 1}
+          />
+        );
+      }
+    }
+
+    if (totalPages > 3 && activePage === 1) {
+      for (let i = 0; i < 3; i++) {
+        pages.push(
+          <Page
+            key={`list-item-${i + 1}`}
+            activePage={activePage}
+            page={i + 1}
+          />
+        );
+      }
+    } else if (totalPages > 3 && activePage === totalPages) {
+      for (let i = totalPages - 3; i < totalPages; i++) {
+        pages.push(
+          <Page
+            key={`list-item-${i + 1}`}
+            activePage={activePage}
+            page={i + 1}
+          />
+        );
+      }
+    } else if (totalPages > 3 && activePage === totalPages - 1) {
+      for (let i = totalPages - 3; i < totalPages; i++) {
+        pages.push(
+          <Page
+            key={`list-item-${i + 1}`}
+            activePage={activePage}
+            page={i + 1}
+          />
+        );
+      }
+    } else if (totalPages > 3 && activePage <= totalPages) {
+      for (let i = activePage - 2; i < activePage + 1; i++) {
+        pages.push(
+          <Page
+            key={`list-item-${i + 1}`}
+            activePage={activePage}
+            page={i + 1}
+          />
+        );
+      }
+    }
+
+    return pages;
   };
 
   render() {
     return (
       <nav className="ter-pagination" role="navigation">
-        <ul className="ter-pagination__list">
-          <li className="ter-pagination__list-item">
-            <a href="/" rel="prev">
-              <i className="fas fa-chevron-left" />
-            </a>
-          </li>
-          {this.generatePageListItems}
-          <li className="ter-pagination__list-item">
-            <a href="/" rel="next">
-              <i className="fas fa-chevron-right" />
-            </a>
-          </li>
-        </ul>
+        {this.props.activePage + 1 > 1 && this.props.activePage > 0 && (
+          <Icon type="open-caret-left-dark-8px" size="8px" />
+        )}
+        <ul className="ter-pagination__list">{this.generatePageListItems()}</ul>
+        {this.props.activePage + 1 < this.props.pages && (
+          <Icon type="open-caret-right-dark-8px" size="8px" />
+        )}
       </nav>
     );
   }
@@ -47,9 +100,20 @@ Pagination.propTypes = {
   pages: PropTypes.arrayOf(
     PropTypes.shape({
       isActive: PropTypes.bool,
-      pageNumber: PropTypes.number.isRequired,
-      // ^^^ Should this be string instead of number? ^^^ //
-      link: PropTypes.string.isRequired
+      pageNumber: PropTypes.number.isRequired
     })
   ).isRequired
+};
+
+export const Page = props => {
+  const { activePage, page } = props;
+  return (
+    <li
+      className={`ter-pagination__list-item ${
+        activePage === page ? "is-active" : ""
+      }`}
+    >
+      {page}
+    </li>
+  );
 };
