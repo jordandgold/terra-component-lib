7d8ee9b2a30d895723ca47ecdb7298c0c80f43d5
Jordan Gold <jordandgold@gmail.com>
2019-04-15 09:16:15 -0600

    setup tabs logic

--- src/lib/Tabs/Tabs.js
+++ src/lib/Tabs/Tabs.js
@@ -3,7 +3,55 @@ import "./Tabs.scss";
 import PropTypes from "prop-types";
 
 class Tabs extends Component {
+  constructor(props) {
+    super(props);
 
+    this.state = {
+      selected: this.props.selected || 0
+    };
+  }
+
+  handleTabChange = index => {
+    this.setState({
+      selected: index
+    });
+  };
+
+  render() {
+    const variantClass = this.props.fullWidth ? "ter-tabs--full-width" : "";
+
+    return (
+      <nav
+        className={`ter-tabs ${variantClass}`}
+        role="navigation"
+        data-controls="#tabs-one"
+      >
+        <ul className="ter-tabs__list" role="tablist">
+          {this.props.children.map((elem, index) => {
+            let selectedClass =
+              index == this.state.selected ? "is-selected" : "";
+            return (
+              <li
+                className={`ter-tabs__list-item ${selectedClass}`}
+                key={index}
+              >
+                <button onClick={event => this.handleTabChange(index)}>
+                  {elem.props.name}
+                </button>
+              </li>
+            );
+          })}
+        </ul>
+        <div className="ter-tabs__content">
+          {this.props.children[this.state.selected]}
+        </div>
+      </nav>
+    );
+  }
 }
 
-export Tabs default;
+export const TabsPanel = props => {
+  return <div>{props.children}</div>;
+};
+
+export default Tabs;
