af084467009692ee4e23ee0931bbff88e426d8c9
Kevin Simpson <kevin_simpson@trimble.com>
2019-04-16 10:13:50 -0600

    Convert Notification to stateless functional component

--- src/lib/Notification/Notification.js
+++ src/lib/Notification/Notification.js
@@ -1,27 +1,25 @@
-import React, { Component } from "react";
+import React from "react";
 import "./Notification.scss";
 import Icon from "../Icon/Icon";
 import PropTypes from "prop-types";
 
-class Notification extends Component {
-  render() {
-    const { onClick, text, type } = this.props;
-    return (
-      <section className={`ter-notification ter-notification--${type}`}>
-        <button
-          className="ter-notification__close"
-          onClick={onClick}
-          aria-label="Close Notification"
-        >
-          <Icon type="enclosed-x-dark-16px" size="16px" />
-        </button>
-        <div class="ter-notification__body">
-          <p>{text}</p>
-        </div>
-      </section>
-    );
-  }
-}
+const Notification = props => {
+  const { onClick, text, type } = props;
+  return (
+    <section className={`ter-notification ter-notification--${type}`}>
+      <button
+        className="ter-notification__close"
+        onClick={onClick}
+        aria-label="Close Notification"
+      >
+        <Icon type="enclosed-x-dark-16px" size="16px" />
+      </button>
+      <div class="ter-notification__body">
+        <p>{text}</p>
+      </div>
+    </section>
+  );
+};
 
 export default Notification;
 
